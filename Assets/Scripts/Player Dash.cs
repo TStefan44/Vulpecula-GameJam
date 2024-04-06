c735940c50de4abbe46d5c65b34a07cf267a05ee
TStefan44 <george.tudose2504@stud.acs.upb.ro>
2024-04-06 22:28:42 +0300

    Dash fix

--- Assets/Scripts/Player Dash.cs
+++ Assets/Scripts/Player Dash.cs
@@ -3,38 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerDash : MonoBehaviour
-{   
-    private bool canDash=true;
-    private bool isDashing=false;
-    public float dashingPower=25f;
-    public float dashingTime=0.2f;
-    public float dashingCooldown=1f; 
+{
+    private bool canDash = true;
+    private bool isDashing = false;
+    public float dashingPower = 25f;
+    public float dashingTime = 0.2f;
+    public float dashingCooldown = 1f;
+    [SerializeField] private GameInput gameInput;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private TrailRenderer tr;
+    //public Text dashType;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        if(Input.GetKeyDown(KeyCode.U))
-        {   
-            Console.WriteLine("U");
-            dashingPower=50f;
-            dashingTime=0.5f;
-            dashingCooldown=2f;
-            tr.startColor=Color.red;
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            //dashType.text = "Long Dash";
+            UnityEngine.Debug.Log("U");
+            dashingPower = 15f;
+            dashingTime = 0.2f;
+            dashingCooldown = 1f;
+            tr.startColor = Color.red;
         }
         if (Input.GetKeyDown(KeyCode.I))
         {
-           Console.WriteLine("I");
-            dashingPower = 25f;
+            //dashType.text = "Normal Dash";
+            UnityEngine.Debug.Log("I");
+            dashingPower = 5f;
             dashingTime = 0.2f;
             dashingCooldown = 1f;
             tr.startColor = Color.blue;
@@ -46,26 +55,37 @@ public class PlayerDash : MonoBehaviour
             return;
         }
 
-        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
         {
             StartCoroutine(Dash());
         }
-        
+
     }
     private IEnumerator Dash()
     {
-        canDash=false;
-        isDashing=true; 
-        float originalGravity=rb.gravityScale;
-        rb.gravityScale= 0f;
-        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
-        tr.emitting=true;
+        float horizontal = gameInput.getHorizontalMovement();
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        //rb.velocity = new Vector2(10000, 0.01f);
+        if (rb.position.x + dashingPower * horizontal > 24.155)
+        {
+            rb.position = new Vector2(24.155f, rb.position.y);
+        }
+        else if (rb.position.x + dashingPower * horizontal < -23.905)
+        {
+            rb.position = new Vector2(-23.905f, rb.position.y);
+        }
+        else
+            rb.position = new Vector2(rb.position.x + dashingPower * horizontal, rb.position.y);
+        tr.emitting = true;
         yield return new WaitForSeconds(dashingTime);
-        tr.emitting=false;
-        rb.gravityScale=originalGravity;
-        isDashing=false;
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
         yield return new WaitForSeconds(dashingCooldown);
-        canDash=true;
+        canDash = true;
 
-    }   
+    }
 }
